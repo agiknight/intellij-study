8c9763c7521c65ec3a851e0202a7cff74afea72d
GitHub Action <action@github.com>
2022-02-09 14:57:45 +0000

    Template cleanup

--- src/main/kotlin/com/github/agiknight/intellijstudy/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/agiknight/intellijstudy/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.agiknight.intellijstudy.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.agiknight.intellijstudy.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/agiknight/intellijstudy/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/agiknight/intellijstudy/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.agiknight.intellijstudy.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.agiknight.intellijstudy.MyBundle
 
 class MyApplicationService {
 
